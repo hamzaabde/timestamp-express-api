d9c3434bf84aef9270126e51b7cdd26451de5788
hamzaabde <xamsze1234@gmail.com>
2022-09-28 11:29:37 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const app = express()
+
+const PORT = 8000
+
+app.get("/api", (req, res) => {
+	const date = new Date()
+
+	res.json({
+		unix: date.valueOf(),
+		utc: date.toUTCString(),
+	})
+})
+
+app.get("/api/:input", (req, res) => {
+	const { input } = req.params
+
+	const date = new Date(input)
+
+	if (isNaN(date.valueOf())) {
+		res.json({
+			error: "Invalid Date",
+		})
+	}
+
+	res.json({
+		unix: date.valueOf(),
+		utc: date.toUTCString(),
+	})
+})
+
+app.listen(PORT, () => {
+	console.log("server running...")
+})
